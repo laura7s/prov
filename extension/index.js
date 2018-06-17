046171593ad7449082a8875c1d8bdb8a2f66f7f0
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-06-17 15:31:42 +0200

    Add better styles

--- extension/index.js
+++ extension/index.js
@@ -8,23 +8,28 @@ const printAsPrettyPercent = value => Math.round(value * 10000) / 100 + "%";
 
 document.addEventListener("DOMContentLoaded", function() {
   document.querySelector("#createHash").addEventListener("click", () => {
-    document.querySelector("#target").textContent = "Loading...";
+    document.querySelector("#loading").textContent = "Loading...";
 
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       var url = tabs[0].url;
       console.log("urL?", url);
 
       compareUrlToKnownArticles(url)
-        .then(({ maxValue, titleForMaxValue }) => {
-          document.querySelector("#target").textContent =
-            "Found max value: " +
-            printAsPrettyPercent(maxValue) +
-            " for article " +
-            titleForMaxValue;
-        })
-        .catch(error => {
-          document.querySelector("#target").textContent = error;
-        });
+        .then(displaySuccess)
+        .catch(displayError);
     });
   });
 });
+
+const displaySuccess = ({ maxValue, titleForMaxValue }) => {
+  document.querySelector("#createHash").remove();
+  document.querySelector("#loading").remove();
+  document.querySelector("#percentage").textContent = printAsPrettyPercent(
+    maxValue
+  );
+  document.querySelector("#title").textContent = "Reuters: " + titleForMaxValue;
+};
+
+const displayError = error => {
+  document.querySelector("#error").textContent = error;
+};
